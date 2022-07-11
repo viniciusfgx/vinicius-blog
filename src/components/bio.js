045eb9ec262aa3c3c3833f6c554a1fb6922eb7f5
viniciusfgx <viniciusfreire187@icloud.com>
2022-07-11 14:19:36 -0300

    feat: add linkedin

--- src/components/bio.js
+++ src/components/bio.js
@@ -19,7 +19,7 @@ const Bio = () => {
             summary
           }
           social {
-            twitter
+            linkedin
           }
         }
       }
@@ -46,8 +46,8 @@ const Bio = () => {
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
           {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
+          <a href={`https://linkedin.com/in/${social?.linkedin || ``}`} target={"_blank"}>
+            Follow me on linkedin
           </a>
         </p>
       )}
